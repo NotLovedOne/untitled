33ce204a9aa73e31f56866414d07cfab06e6b4b0
NotLovedOne <fistofsnake@gmail.com>
2025-08-11 17:33:05 +0500

    commit: Update Fibonacci and Factorial methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Fibonacci of 8: " + new Main().fibonacci(8));
+        // System.out.println("Fibonacci of 9: " + new Main().fibonacci(9));
+        System.out.println("Factorial of 5: " + new Main().factorial(5));
     }
 
     public int fibonacci(int n) {
@@ -12,5 +13,12 @@ public class Main {
             return 1;
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
 }
